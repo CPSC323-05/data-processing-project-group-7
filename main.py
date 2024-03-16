14f5f1f4ae564137897d20c1bbfc7e5b7390ada0
HowVo <146135155+HowVo@users.noreply.github.com>
2024-03-16 00:14:18 -0700

    Update main.py
    remove blank lines
    regular expressions for different types of tokens added

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ with open("test.txt", "r") as file:
 # Split the content into a list of words using newline as delimiter
 words_list = word.split("\n")
 
-# List comprehension to remove all items that contain '#'; i.e all comments
+# Remove blank lines
+words_list = filter(lambda line: line.strip(), words_list)
+
+# List comprehension to remove all items that contain '#' and " "; i.e all comments and white spaces
 clean_list = [item for item in words_list if '#' not in item]
+clean_list = [" ".join(item.split()) for item in clean_list]
 
 # print (clean_list)
 
 # Print remaining items in list
 for item in clean_list:
     print(item)
+
+# Define regular expressions for different types of tokens
+patterns = [
+    (r'\d+|".*?"', 'LITERAL'), # Literal
+    (r'\b(?:if|else|for|while|def|return)\b', 'KEYWORD'),  # Keyword
+    (r'\b[a-zA-Z_]\b', 'IDENTIFIER'),  # Identifier
+    (r'\(|\)|\[|\]|\{|\}|,|\.|;|:', 'PUNCTUATION'),  # Punctuation
+    (r'\+|-|\*|/|%|==|!=|<|>|<=|>=|=|\+=|-=|\*=|/=|%=|\+\+|--', 'OPERATOR')  # Operator
+]
+
+# Check each item of "clean_list" to see what regex pattern it matches using for loop
