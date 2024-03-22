ee9a8bcae8de49b5e92c267368507acc34e5c694
victor prieto <vp@MSI>
2024-03-22 15:17:34 -0700

    Functions made, check comments at bottom

--- main.py
+++ main.py
@@ -40,3 +40,61 @@ for pattern, token in patterns:
 # Print the lexemes and their tokens
 for lexeme, token in result:
     print(lexeme + ": " + token)
+
+
+# import re
+
+# # Function to open file and read its content.
+# def open_file(filename):
+#     with open(filename, 'r') as file:
+#         words = file.read()
+#         return words
+    
+# # Function to create list to hold all lines
+# def create_list(code):
+#     words_list = code.split("\n")
+#     words_list = filter(lambda line: line.strip(), words_list) # Remove blank lines
+#     return  words_list
+
+# # Function to clean up comments and excess spaces
+# def cleanup_lines(code):
+#     clean_list = [item for item in code if '#' not in item] 
+#     clean_list = [" ".join(item.split()) for item in clean_list]
+#     return clean_list
+
+# def pattern(clean):
+#     patterns = [
+#     (r'\d+|".*?"', 'LITERAL'), # Literal
+#     (r'\b(?:if|else|for|while|def|return)\b', 'KEYWORD'),  # Keyword
+#     (r'\b[a-zA-Z_]\b', 'IDENTIFIER'),  # Identifier
+#     (r'\(|\)|\[|\]|\{|\}|,|\.|;|:', 'PUNCTUATION'),  # Punctuation
+#     (r'\+|-|\*|/|%|==|!=|<|>|<=|>=|=|\+=|-=|\*=|/=|%=|\+\+|--', 'OPERATOR')  # Operator
+#     ]
+    
+#     result = []
+#     for pattern, token in patterns:
+#         for item in clean:
+#             for match in re.finditer(pattern, item):
+#                 result.append((match.group(), token))
+                
+#     return result
+    
+# def main():
+#     input_code = open_file('test.txt')
+    
+#     code_list = create_list(input_code)
+    
+#     clean_list = cleanup_lines(code_list)
+    
+#     for item in clean_list:
+#         print(item)
+        
+#     print() # Blank space
+    
+#     clean_list = pattern(clean_list)
+    
+#     for lexemme, token in clean_list:
+#         print(lexemme + ": " + token)
+
+# if __name__ == "__main__":
+#     main()
